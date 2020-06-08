0d9c5b5caf6fd62cdb9e62b28af94770ad38b995
Andrea Napoletani <andrea.napoletani@gmail.com>
2020-06-08 12:17:21 +0200

    Added skybox and first robot animation

--- js/cameraMovement.js
+++ js/cameraMovement.js
@@ -4,12 +4,12 @@ import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
 import {AnimateRobot} from './robot-animations.js';
 import {KillingRobot} from './robot.js'
 
-var scene, camera, mesh;
-var meshFloor;
+var scene, camera, robot;
 
 var keyboard = {};
 var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
 var USE_WIREFRAME = false;
+
 
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({canvas});
@@ -18,6 +18,7 @@ function init(){
 
 
 	scene = new THREE.Scene();
+	scene.background = new THREE.Color('white');
 	const fov = 45;
 	const aspect = 2;  // the canvas default
 	const near = 1;
@@ -53,7 +54,7 @@ function init(){
 	mesh.receiveShadow = true;
 	scene.add(mesh);
     
-    const robot = new KillingRobot();
+    robot = new KillingRobot();
     robot.castShadow = true;
     robot.receiveShadow = true;
     scene.add(robot);
@@ -65,13 +66,54 @@ function init(){
 	
 	camera.position.set(0, player.height, -5);
 	camera.lookAt(new THREE.Vector3(0,player.height,0));
+
+	let materialArray = [];
+
+	let texture_ft = new THREE.TextureLoader().load( 'js/bg_images/arid2_ft.jpg');
+	let texture_bk = new THREE.TextureLoader().load( 'js/bg_images/arid2_bk.jpg');
+	let texture_up = new THREE.TextureLoader().load( 'js/bg_images/arid2_up.jpg');
+	let texture_dn = new THREE.TextureLoader().load( 'js/bg_images/arid2_dn.jpg');
+	let texture_rt = new THREE.TextureLoader().load( 'js/bg_images/arid2_rt.jpg');
+	let texture_lf = new THREE.TextureLoader().load( 'js/bg_images/arid2_lf.jpg');
+
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+	materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+	
+	texture_ft.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	texture_bk.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	texture_up.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	texture_dn.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	texture_rt.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	texture_lf.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+	materialArray.map.magFilter = THREE.LinearMipMapLinearFilter;
+	/*materialArray[0].map.magFilter = THREE.LinearMipMapLinearFilter;
+	materialArray[1].map.magFilter = THREE.LinearMipMapLinearFilter;
+	materialArray[2].map.magFilter = THREE.LinearMipMapLinearFilter;
+	materialArray[3].map.magFilter = THREE.LinearMipMapLinearFilter;
+	materialArray[4].map.magFilter = THREE.LinearMipMapLinearFilter;
+	materialArray[5].map.magFilter = THREE.LinearMipMapLinearFilter;*/
+
+
+	for (let i = 0; i < 6; i++){
+		materialArray[i].side = THREE.BackSide;;
+	}
+	let skyboxGeo = new THREE.BoxGeometry( 4000, 4000, 4000);
+	let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+
+	scene.add( skybox );
 	
 	if (resizeRendererToDisplaySize(renderer)) {
 		const canvas = renderer.domElement;
 		camera.aspect = canvas.clientWidth / canvas.clientHeight;
 		camera.updateProjectionMatrix();
 	}
-	
+
+
 	animate();
 }
 
@@ -87,6 +129,7 @@ function resizeRendererToDisplaySize(renderer) {
 }
 
 function animate(){
+	
 	requestAnimationFrame(animate);
 	
 	//mesh.rotation.x += 0.01;
@@ -118,6 +161,10 @@ function animate(){
 	if(keyboard[39]){ // right arrow key
 		camera.rotation.y += player.turnSpeed;
 	}
+
+	AnimateRobot(robot);
+
+	TWEEN.update();
 	
 	renderer.render(scene, camera);
 }
